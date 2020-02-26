f7cf2d5ead98d51ac3b8d2b19899c22d9064797a
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-02-26 09:55:53 +0000

    Add styling to calculator

---
+++ Assets/js/main.js
@@ -0,0 +1,8 @@
+let display = document.getElementById("display")
+let buttons = document.getElementsByClassName("btn")
+
+for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    console.log(button);
+    
+}
